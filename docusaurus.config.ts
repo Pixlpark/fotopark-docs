85cbd985d1ba146d6f60d905e0b1f184aa4da4e7
malyshkov <a.malyshkov@pixlpark.com>
2025-06-16 17:43:27 +0700

    2

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,7 +3,7 @@ import type { Config } from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-    title: 'Документация Pixlpark',
+    title: 'Документация Мой выпускной',
     tagline: 'Documentation',
     favicon: 'img/favicon.ico',
 

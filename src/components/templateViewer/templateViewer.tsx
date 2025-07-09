943220bf033183d30dcf45c9e9d766b317c4a69b
h1karoo <o.filipow@yandex.ru>
2025-07-09 13:34:49 +0700

    fix templates

--- src/components/templateViewer/templateViewer.tsx
+++ src/components/templateViewer/templateViewer.tsx
@@ -1,96 +1,9 @@
-// import { useState, useEffect } from 'react';
-
-// const TemplateViewer = () => {
-//   const [templates, setTemplates] = useState([]);
-//   const [selectedTemplate, setSelectedTemplate] = useState(null);
-//   const [isModalOpen, setIsModalOpen] = useState(false);
-
-//   useEffect(() => {
-//     const fetchTemplates = async () => {
-//       const apiUrl = 'https://moyvipusknoy.ru/api/templateSets';
-//       const requestBody = {
-//         materialTypeId: 0,
-//         materialIds: [11721572],
-//         authorId: 0,
-//         languageId: 8166,
-//         ownType: 0,
-//         page: 0,
-//         pageSize: 50,
-//         tagIds: [],
-//         tagUrl: ""
-//       };
-
-//       try {
-//         const response = await fetch(apiUrl, {
-//           method: 'POST',
-//           headers: {
-//             'Content-Type': 'application/json'
-//           },
-//           body: JSON.stringify(requestBody)
-//         });
-//         const data = await response.json();
-//         setTemplates(data.templates || []);
-//       } catch (err) {
-//         console.error('Ошибка загрузки шаблонов:', err);
-//       }
-//     };
-
-//     fetchTemplates();
-//   }, []);
-
-//   const openModal = (templateSet) => {
-//     setSelectedTemplate(templateSet);
-//     setIsModalOpen(true);
-//   };
-
-//   const closeModal = () => {
-//     setIsModalOpen(false);
-//     setSelectedTemplate(null);
-//   };
-
-//   return (
-//     <div className="template-viewer">
-//       <h2>Шаблоны</h2>
-//       <div className="grid">
-//         {templates.map((template) => (
-//           <img
-//             key={template.Id}
-//             src={`https://moyvipusknoy.ru${template.CoverUrl}`}
-//             className="template"
-//             alt={`Шаблон ${template.Name}`}
-//             onClick={() => openModal(template)}
-//           />
-//         ))}
-//       </div>
-
-//       {isModalOpen && selectedTemplate && (
-//         <div className="modal">
-//           <div className="modal-content">
-//             <button className="close-btn" onClick={closeModal}>
-//               ×
-//             </button>
-//             <div className="modal-images">
-//               {selectedTemplate.Templates.map((template) => (
-//                 <img
-//                   key={template.Id}
-//                   src={`https://moyvipusknoy.ru/content/pxp-template-cover/${template.Id}.png?pid=6830&v=${template.Hash}&size=S`}
-//                   alt={`Шаблон ${template.Name}`}
-//                 />
-//               ))}
-//             </div>
-//           </div>
-//         </div>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default TemplateViewer;
 import { useState, useEffect } from 'react';
 
 const TemplateViewer = ({ 
   baseUrl = 'https://moyvipusknoy.ru',
-  materialIds = [11721572]
+  materialIds = [11721572],
+  pid = 6830 // Новый пропс с значением по умолчанию
 }) => {
   const [templates, setTemplates] = useState([]);
   const [selectedTemplate, setSelectedTemplate] = useState(null);
@@ -164,7 +77,7 @@ const TemplateViewer = ({
               {selectedTemplate.Templates.map((template) => (
                 <img
                   key={template.Id}
-                  src={`${baseUrl}/content/pxp-template-cover/${template.Id}.png?pid=6830&v=${template.Hash}&size=S`}
+                  src={`${baseUrl}/content/pxp-template-cover/${template.Id}.png?pid=${pid}&v=${template.Hash}&size=S`}
                   alt={`Шаблон ${template.Name}`}
                 />
               ))}
